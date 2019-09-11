c71d7e3887170a45800e8292071d9beb34c30ed6
Sam Costanzo <samcostanzo25@gmail.com>
2019-09-11 17:37:25 -0400

    Fixed font color issue on error pages

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,6 @@ import React from 'react';
 const NotFound = () => {
     return(
         <li className="not-found">
-            <h3>Oh no!</h3>
             <p>We couldn't find anything that matched your search. Sorry!</p>
         </li>
     );
--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageNotFound = () => {
     return(
-        <div>
+        <div className="page-not-found">
             <h1>Whoops! This page was not found. Please check the URL above.</h1>
         </div>
     );
